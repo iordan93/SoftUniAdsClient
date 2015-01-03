9186d74a94828a2efc7a643532d77cd4b87f6d95
Yordan Darakchiev <iordan93@gmail.com>
2015-01-03 17:38:57 +0200

    Basic home page setup

---
+++ SoftUniAdsClient/SoftUniAdsClient/scripts/app.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module("softUniAds", ["ngRoute"]);
+app.value("toastr", toastr)
+    .constant("baseUrl", "http://localhost:1337/")
+    .config(["$routeProvider", function ($routeProvider) {
+        $routeProvider.
+          when("/", {
+              templateUrl: "views/partials/home.html",
+              controller: "HomeController"
+          }).
+          otherwise({
+              redirectTo: "/"
+          });
+    }]);
