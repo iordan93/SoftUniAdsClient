487e95ef1e27075a185493cbc179fa814361b5b1
Yordan Darakchiev <iordan93@gmail.com>
2015-01-09 12:16:54 +0200

    Added resources for categories and towns

---
+++ SoftUniAdsClient/SoftUniAdsClient/scripts/services/resources/TownsResource.js
@@ -0,0 +1,9 @@
+﻿app.factory("TownsResource", ["$resource", "baseUrl", function ($resource, baseUrl) {
+    var townsResource = $resource(baseUrl + "towns");
+
+    return {
+        all: function (success, error) {
+            return townsResource.query(success, error);
+        }
+    };
+}]);
