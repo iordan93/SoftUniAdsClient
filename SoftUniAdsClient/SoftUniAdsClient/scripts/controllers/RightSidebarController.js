43c4e4fc0b139acada029deb509108a91b4a4324
Yordan Darakchiev <iordan93@gmail.com>
2015-01-09 22:45:34 +0200

    Made categories and towns selectable

--- SoftUniAdsClient/SoftUniAdsClient/scripts/controllers/RightSidebarController.js
+++ SoftUniAdsClient/SoftUniAdsClient/scripts/controllers/RightSidebarController.js
@@ -1,5 +1,14 @@
 ﻿app.controller("RightSidebarController", ["$scope", "$rootScope", "CategoriesResource", "TownsResource",
     function ($scope, $rootScope, CategoriesResource, TownsResource) {
         $scope.categories = CategoriesResource.all();
+        $scope.selectedCategoryId = null;
+        $scope.selectCategory = function (id) {
+            $scope.selectedCategoryId = id;
+        };
+
         $scope.towns = TownsResource.all();
+         $scope.selectedTownId = null;
+        $scope.selectTown = function (id) {
+            $scope.selectedTownId = id;
+        };
 }]);
